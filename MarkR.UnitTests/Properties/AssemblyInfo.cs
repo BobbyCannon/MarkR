983f9aadaf72b4c82e6345c14b40510bfffe6d28
Bobby Cannon <bobby.cannon@live.com>
2015-10-11 19:15:59 -0400

    Posting for final build.

--- MarkR.UnitTests/Properties/AssemblyInfo.cs
+++ MarkR.UnitTests/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.14.5762.40882")]
-[assembly: AssemblyFileVersion("1.14.5762.40882")]
+[assembly: AssemblyVersion("1.14.5762.41862")]
+[assembly: AssemblyFileVersion("1.14.5762.41862")]
